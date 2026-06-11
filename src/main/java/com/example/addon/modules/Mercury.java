1a671643b321585806a2861feb876f1be85c6f59
tommyfart <135668188+tommyfart@users.noreply.github.com>
2026-06-10 18:26:50 -0600

    Update Mercury.java

--- src/main/java/com/example/addon/modules/Mercury.java
+++ src/main/java/com/example/addon/modules/Mercury.java
@@ -1,48 +1,50 @@
 package com.example.addon.modules;
 
 import com.example.addon.AddonTemplate;
+
+import meteordevelopment.meteorclient.settings.BoolSetting;
+import meteordevelopment.meteorclient.settings.IntSetting;
+import meteordevelopment.meteorclient.settings.Setting;
+import meteordevelopment.meteorclient.settings.SettingGroup;
 import meteordevelopment.meteorclient.systems.modules.Module;
-import meteordevelopment.meteorclient.settings.*;
 
 public class Mercury extends Module {
+    private final SettingGroup sgGeneral = settings.getDefaultGroup();
 
-    public final SettingGroup sgGeneral = settings.getDefaultGroup();
-
-    public final Setting<Boolean> storageEsp = sgGeneral.add(new BoolSetting.Builder()
-        .name("storage-esp")
-        .defaultValue(true)
-        .build()
+    private final Setting<Boolean> storageEsp = sgGeneral.add(
+        new BoolSetting.Builder()
+            .name("storage-esp")
+            .defaultValue(true)
+            .build()
     );
 
-    public final Setting<Boolean> tracers = sgGeneral.add(new BoolSetting.Builder()
-        .name("storage-tracers")
-        .defaultValue(true)
-        .build()
+    private final Setting<Boolean> tracers = sgGeneral.add(
+        new BoolSetting.Builder()
+            .name("tracers")
+            .defaultValue(true)
+            .build()
     );
 
-    public final Setting<Boolean> susChunks = sgGeneral.add(new BoolSetting.Builder()
-        .name("sus-chunks")
-        .defaultValue(true)
-        .build()
+    private final Setting<Boolean> stashAlerts = sgGeneral.add(
+        new BoolSetting.Builder()
+            .name("stash-alerts")
+            .defaultValue(true)
+            .build()
     );
 
-    public final Setting<Integer> range = sgGeneral.add(new IntSetting.Builder()
-        .name("range")
-        .defaultValue(128)
-        .min(32)
-        .max(512)
-        .build()
-    );
-
-    public final Setting<Integer> threshold = sgGeneral.add(new IntSetting.Builder()
-        .name("chunk-threshold")
-        .defaultValue(25)
-        .min(1)
-        .max(100)
-        .build()
+    private final Setting<Integer> threshold = sgGeneral.add(
+        new IntSetting.Builder()
+            .name("stash-threshold")
+            .defaultValue(25)
+            .range(1, 100)
+            .build()
     );
 
     public Mercury() {
-        super(AddonTemplate.CATEGORY, "Mercury", "Main stash finder control module.");
+        super(
+            AddonTemplate.CATEGORY,
+            "mercury",
+            "Base and stash finder."
+        );
     }
 }
