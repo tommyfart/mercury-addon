8bed36f79faaecf826d6c25aa66ca5022281c4e6
tommyfart <135668188+tommyfart@users.noreply.github.com>
2026-06-10 18:12:57 -0600

    Refactor AddonTemplate to include new modules

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -1,35 +1,30 @@
 package com.example.addon;
 
-import com.example.addon.commands.CommandExample;
-import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ModuleExample;
+import com.example.addon.modules.Mercury;
+import com.example.addon.modules.StorageEsp;
+import com.example.addon.modules.StorageTracers;
+import com.example.addon.modules.SusChunks;
+
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.commands.Commands;
-import meteordevelopment.meteorclient.systems.hud.Hud;
-import meteordevelopment.meteorclient.systems.hud.HudGroup;
-import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
 
 public class AddonTemplate extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+
+    public static final meteordevelopment.meteorclient.systems.modules.Category CATEGORY =
+        new meteordevelopment.meteorclient.systems.modules.Category("Mercury");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
+        LOG.info("Initializing Mercury Addon");
 
-        // Modules
-        Modules.get().add(new ModuleExample());
-
-        // Commands
-        Commands.add(new CommandExample());
-
-        // HUD
-        Hud.get().register(HudExample.INFO);
+        Modules.get().add(new Mercury());
+        Modules.get().add(new StorageEsp());
+        Modules.get().add(new StorageTracers());
+        Modules.get().add(new SusChunks());
     }
 
     @Override
